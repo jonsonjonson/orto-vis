a098f122054673d1c55ee24c2b1eaf3e0b72d35f
Mateusz Pietrzak <mateusz.pietrzak@outlook.com>
2015-12-26 03:16:50 +0100

    Remade prosthetics

--- src/app/components/prosthetics/prosthetics.ts
+++ src/app/components/prosthetics/prosthetics.ts
@@ -6,7 +6,11 @@ import {Component, View} from 'angular2/angular2';
   selector: 'prosthetics'
 })
 @View({
-  template: require('./prosthetics.html')
+  template: require('./prosthetics.html'),
+  styles: [
+    require('../../styles/helpers.scss'),
+    require('../../styles/main.scss')
+  ]
 })
 
 export class Prosthetics { }
