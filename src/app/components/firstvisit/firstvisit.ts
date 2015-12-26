f49cb5d9aa0a66a6b5d4adc2c91cb147f23d06b7
Mateusz Pietrzak <mateusz.pietrzak@outlook.com>
2015-12-26 02:10:18 +0100

    Added styles to first visit page

--- src/app/components/firstvisit/firstvisit.ts
+++ src/app/components/firstvisit/firstvisit.ts
@@ -6,7 +6,11 @@ import {Component, View} from 'angular2/angular2';
   selector: 'firstvisit'
 })
 @View({
-  template: require('./firstvisit.html')
+  template: require('./firstvisit.html'),
+  styles: [
+    require('../../styles/helpers.scss'),
+    require('../../styles/main.scss')
+  ]
 })
 
 export class FirstVisit { }
