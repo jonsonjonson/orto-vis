385be0058b437352e432599c9c57f7511d77bf42
Mateusz Pietrzak <mateusz.pietrzak@outlook.com>
2015-12-26 12:40:53 +0100

    Added contact page

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -6,7 +6,11 @@ import {Component, View} from 'angular2/angular2';
   selector: 'contact'
 })
 @View({
-  template: require('./contact.html')
+  template: require('./contact.html'),
+  styles: [
+    require('../../styles/helpers.scss'),
+    require('../../styles/main.scss')
+  ]
 })
 
 export class Contact { }
