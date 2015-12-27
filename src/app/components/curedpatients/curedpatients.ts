362570f00e205930408a73a5d02bebf23126424b
Mateusz Pietrzak <mateusz.pietrzak@outlook.com>
2015-12-27 03:20:58 +0100

    Remade cured patients gallery

--- src/app/components/curedpatients/curedpatients.ts
+++ src/app/components/curedpatients/curedpatients.ts
@@ -6,7 +6,13 @@ import {Component, View} from 'angular2/angular2';
   selector: 'curedpatients'
 })
 @View({
-  template: require('./curedpatients.html')
+  template: require('./curedpatients.html'),
+  styles: [
+    require('common/styles/helpers.scss'),
+    require('common/styles/main.scss'),
+    require('./styles/curedpatient.scss'),
+    require('./styles/curedpatients-gallery.scss')
+  ]
 })
 
 export class CuredPatients { }
